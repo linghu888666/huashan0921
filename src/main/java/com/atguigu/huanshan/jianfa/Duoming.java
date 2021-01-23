4e828dd5152125857d9f1ca3a48af0eaf0145281
linghu888666 <linghu888666@126.com>
2021-01-23 12:35:42 +0800

    增加三仙剑

---
+++ src/main/java/com/atguigu/huanshan/jianfa/Duoming.java
@@ -0,0 +1,10 @@
+package com.atguigu.huanshan.jianfa;
+
+public class Duoming {
+    public static void main(String[] args) {
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+    }
+
+}
